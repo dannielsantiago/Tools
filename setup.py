1f9d19b5252a271d5cb5959ebe6ca77e616bddb9
Daniel S. Penagos Molina <santiago.penagos@uni-jena.de>
2023-07-26 10:37:30 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='IAP',
+    version='0.1.0',
+    author='DSPM',
+    packages=[
+        "IAP","IAP.Tools",
+    ],
+    install_requires=[
+        'scikit-image','scipy','matplotlib','numpy',
+    ],
+)
